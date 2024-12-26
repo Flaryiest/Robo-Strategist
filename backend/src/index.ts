6aa678c5f5a38fa6a8ba81bfaf89a83c802eec1e
Flaryiest <ericmzuo1@gmail.com>
2024-12-25 20:04:33 -0700

    initated project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,28 @@
-import express, { Request, Response } from "express"
-const app = express()
-const port = 3000
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello!")
-})
+import * as dotenv from "dotenv";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import cookieParser  from "cookie-parser";
+import helmet from "helmet";
+dotenv.config();
+
+const app = express();
+const port: number = parseInt(process.env.PORT || "3000", 10);
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+app.use(helmet());
+
+app.get("/", (req: Request, res: Response): void => {
+  console.log("test");
+  res.send("Received Request");
+});
 
 app.listen(port, () => {
-    console.log("App listening at ${port}")
-})
+  console.log(`Listening on port: ${port}`);
+});
